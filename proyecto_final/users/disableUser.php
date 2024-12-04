ce6cdff5f3ad446e4b6102c040e6833b2913283a
POLOK8039 <anonymous.pamb@gmail.com>
2024-12-03 21:10:14 -0800

    Add files via upload

---
+++ proyecto_final/users/disableUser.php
@@ -0,0 +1,19 @@
+<?php
+    require_once("../config.php");
+    require_once("userFun.php");
+
+    if (validateUser("ADMIN") && isset($_GET['num'])) {
+        $num = $_GET['num'];
+        
+        if (disableUser($num)) {
+            echo "user successfully disabled.";
+        } else {
+            echo "Error deactivating user.";
+        }
+
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "User code not provided.";
+    }
+?>
